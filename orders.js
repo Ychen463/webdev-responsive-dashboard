5c71c42bda73e37ae7557527fe3e5756c1beb7da
ychen463 <gisellec2@163.com>
2022-08-29 18:00:13 -0400

    commit db

---
+++ orders.js
@@ -0,0 +1,43 @@
+const Orders = [{
+        productName: 'Foldable Mini Drone',
+        productNumber: '92834',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '23459',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '29357',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Dron with Camera 2 Drone',
+        productNumber: '03948',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4k Dron',
+        productNumber: '12394',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'DJI Air 2S',
+        productNumber: '93847',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Dronenge Dron',
+        productNumber: '00284',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+]
